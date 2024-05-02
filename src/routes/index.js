2ced43b56c1f1c50d66c7fcd19b0b3471d823f02
Gaspari <lcsgaspari@gmail.com>
2024-05-01 22:02:29 -0300

    adding a register form, a controller structure to send data to database and rotes to all work properly

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 const router = require('express').Router()
 
+const CustomersController = require('../controllers/customers')
+
 //Rotas
 router.get('/', (req, res) => {
     res.render('index', {
         title: 'Teste CRUD'
     })
 })
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Cadastro de clientes'
+    })
+})
+
+router.post('/register/add', CustomersController.add)
 
 module.exports = router
